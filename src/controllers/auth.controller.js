c42b567052d614226ba6b7416d47145389c2b7aa
Pablo Coronel <pablomanuelcoronel@gmail.com>
2021-02-15 18:09:18 -0300

    feat: create token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import User from '../models/User';
+import jwt from 'jsonwebtoken';
+import config from '../config';
 
 export const singUp = async (req, res) => {
 	const { username, email, password } = req.body;
@@ -9,9 +11,14 @@ export const singUp = async (req, res) => {
 		password: await User.encryptPassword(password),
 	});
 
-	await user.save();
+	const savedUser = await user.save();
 
-	res.json('singup');
+	// crear token
+	const token = jwt.sign({ id: savedUser._id }, config.SECRET, {
+		expiresIn: '24h',
+	});
+
+	res.status(200).json({ token });
 };
 export const singIn = async (req, res) => {
 	res.json('singin');
