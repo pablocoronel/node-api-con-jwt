5a881c4ed62b834f6367a162d56be5868165a29a
Pablo Coronel <pablomanuelcoronel@gmail.com>
2021-02-15 19:04:18 -0300

    feat: add role at create new user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,17 +1,29 @@
 import User from '../models/User';
 import jwt from 'jsonwebtoken';
 import config from '../config';
+import Role from '../models/Role';
 
 export const singUp = async (req, res) => {
-	const { username, email, password } = req.body;
+	const { username, email, password, roles } = req.body;
 
-	const user = new User({
+	const newUser = new User({
 		username,
 		email,
 		password: await User.encryptPassword(password),
 	});
 
-	const savedUser = await user.save();
+	if (roles) {
+		// Para roles enviados en request
+		const foundRoles = await Role.find({ name: { $in: roles } });
+
+		newUser.roles = foundRoles.map((role) => role._id);
+	} else {
+		const userRole = await Role.findOne({ name: 'user' });
+		newUser.roles = [userRole._id];
+	}
+
+	const savedUser = await newUser.save();
+	console.log(savedUser);
 
 	// crear token
 	const token = jwt.sign({ id: savedUser._id }, config.SECRET, {
