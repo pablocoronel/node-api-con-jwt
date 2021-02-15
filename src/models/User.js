ab4e474a97850af8d4f96fbac5e630812c9b0d59
Pablo Coronel <pablomanuelcoronel@gmail.com>
2021-02-15 17:17:07 -0300

    feat: add functions for password

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import bcryp from 'bcryptjs';
 
 const UserSchema = new Schema(
 	{
@@ -17,5 +18,17 @@ const UserSchema = new Schema(
 		versionKey: false,
 	}
 );
+
+// Funciones para el modelo
+// Encripta las contraseña
+UserSchema.statics.encryptPassword = async (password) => {
+	// Salt sirve para indicar la cantidad de veces a ejecutar el algoritmo de encriptación
+	return await bcryp.hash(password, bcryp.genSaltSync(10));
+};
+
+// Compara una la contraseña del modelo ante un string recibido
+UserSchema.statics.comparePassword = async (password, receivedPassword) => {
+	return await bcryp.compare(password, receivedPassword);
+};
 
 export default model('User', UserSchema);
