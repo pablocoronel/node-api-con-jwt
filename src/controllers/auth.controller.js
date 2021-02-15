78ee119cbe113113d4884a47b6ee68c6b604af91
Pablo Coronel <pablomanuelcoronel@gmail.com>
2021-02-15 17:20:20 -0300

    feat: create user in singUp route

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,16 @@
 import User from '../models/User';
 
 export const singUp = async (req, res) => {
+	const { username, email, password } = req.body;
+
+	const user = new User({
+		username,
+		email,
+		password: await User.encryptPassword(password),
+	});
+
+	await user.save();
+
 	res.json('singup');
 };
 export const singIn = async (req, res) => {
