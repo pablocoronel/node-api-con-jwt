415824e89f542323888af906cd018a9644cd7d80
Pablo Coronel <pablomanuelcoronel@gmail.com>
2021-02-15 23:47:53 -0300

    feat: use try catch for verify token

--- src/middlewares/authJWT.js
+++ src/middlewares/authJWT.js
@@ -3,24 +3,28 @@ import config from '../config';
 import User from '../models/User';
 
 const verifyToken = async (req, res, next) => {
-	const token = req.headers['x-access-token'];
+	try {
+		const token = req.headers['x-access-token'];
 
-	console.log(token);
+		console.log(token);
 
-	if (!token) {
-		return res.status(403).json({ message: 'No token provided' });
-	} else {
-		const decoded = jwt.verify(token, config.SECRET);
-		req.userId = decoded.id;
+		if (!token) {
+			return res.status(403).json({ message: 'No token provided' });
+		} else {
+			const decoded = jwt.verify(token, config.SECRET);
+			req.userId = decoded.id;
 
-		const user = await User.findById(decoded.id, { password: 0 }); // No enviar la contraseña
-        console.log(user)
-		if (!user) {
-			return res.status(404).json({ message: 'No user found' });
+			const user = await User.findById(decoded.id, { password: 0 }); // No enviar la contraseña
+			console.log(user);
+			if (!user) {
+				return res.status(404).json({ message: 'No user found' });
+			}
 		}
-	}
 
-	next();
+		next();
+	} catch (error) {
+		return res.status(401).json({ message: 'Unauthorized' });
+	}
 };
 
 export { verifyToken };
